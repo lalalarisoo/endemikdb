707f32c6be0e30bceca8fd839e9f3bc873c24c9d
lalalarisoo <idelialarisa01@gmail.com>
2025-06-29 21:51:58 +0700

    Update endemik_card.dart

--- lib/widget/endemik_card.dart
+++ lib/widget/endemik_card.dart
@@ -4,25 +4,36 @@ import '../pages/detail_page.dart';
 
 class EndemikCard extends StatelessWidget {
   final Endemik item;
-  const EndemikCard(this.item, {super.key});
+  final bool isFavorite;
+
+  const EndemikCard(this.item, {super.key, this.isFavorite = false});
 
   @override
   Widget build(BuildContext context) {
+    final favProvider = Provider.of<FavoriteProvider>(context, listen: false);
+
     return Card(
-      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
       child: ListTile(
         leading: Image.network(item.foto, width: 60, fit: BoxFit.cover),
         title: Text(item.nama),
         subtitle: Text(item.nama_latin),
-        trailing: item.is_favorit == "true"
-            ? const Icon(Icons.favorite, color: Colors.red)
-            : const Icon(Icons.favorite_border),
+        trailing: IconButton(
+          icon: Icon(
+            isFavorite ? Icons.favorite : Icons.favorite_border,
+            color: Colors.red,
+          ),
+          onPressed: () {
+            if (isFavorite) {
+              favProvider.removeFavorite(item);
+            } else {
+              favProvider.addFavorite(item);
+            }
+          },
+        ),
         onTap: () {
           Navigator.push(
             context,
-            MaterialPageRoute(
-              builder: (_) => DetailPage(item: item),
-            ),
+            MaterialPageRoute(builder: (_) => DetailPage(item: item)),
           );
         },
       ),
